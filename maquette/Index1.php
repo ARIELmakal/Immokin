1a6e003ccb05cb6a60fef219c977163a68553a61
Arma <141159690+ARIELmakal@users.noreply.github.com>
2023-08-11 08:43:43 +0100

    Add files via upload
    Voici ma page web

---
+++ maquette/Index1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+    <link rel="stylesheet" href="CSS/style1.css">
+</head>
+<body>
+    <header><p style="text-align: center;">IMMOKIN</p></header>
+
+    <main>
+        <div class="div">
+            <span><h3 class="accueil"><a href="#">Acceuil</a></h5></span>
+            <span><h3 class="Inscriptions"><a href="#">Inscriptions</a></h3></span>
+            <span><h3 class="plus"><a href="#">A propos </a></h3></span>
+            <span><h3 class="join"><a href="">Rejoignez-nous</a></h3></span>
+            
+        </div>
+        <div class="div2">
+            <h1>Mon agence immobolière</h1>
+            <strong><cite>Pour vivre heureux, il vous suffit de bien vivre et pour vivre longtemps, quoi de mieux que de rester chez soi.</cite></strong>
+        </div>
+        <div class><p style="text-align: center;">Vous êtes à la recherche du cadre idéal ?</p></div>
+    </main>
+        
+    <footer>
+        <hr>
+        <div>
+        <span>Avez-vous un compte cliquez <a href="rejoins.html">ici</a></span>
+        </div>
+    </footer>  
+</body>
+</html>
